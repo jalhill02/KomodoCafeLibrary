e0808dcb731b0dbe8b66de312b886dd218d5b3be
Jamie Hill <jalhill01@gmail.com>
2020-12-17 03:29:16 -0500

    set up my unit testfor 03

---
+++ _03_KomodoInsUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("_03_KomodoInsUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_03_KomodoInsUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e760a5ae-541a-4cb2-a43c-e8e94d5a858e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ _03_KomodoInsUnitTest/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using _03_KomodoInsuranceRepository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _03_KomodoInsUnitTest
+{
+   
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_AddToDitctionary()
+        {
+            //Arrange -> Set things up.
+            //Check with Terry to be sure I am using the right Ref. of _o3_KomodoInsuranceRepo of 
+            KomodoInsuranceRepo _kIRepo = new KomodoInsuranceRepo();
+            Badges badges = new Badges(); // need to fill in parameters
+
+
+
+            //Act - Do some work
+
+            //Assert check to see if the work is working
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod_GetBadges()
+        {
+            //Arrange -> Set things up.
+
+            //Act - Do some work
+
+            //Assert check to see if the work is working
+
+        }
+
+        [TestMethod]
+        public void TestMethod_EditBadgeInfo()
+        {
+            //Arrange -> Set things up.
+
+            //Act - Do some work
+
+            //Assert check to see if the work is working
+
+        }
+
+        [TestMethod]
+        public void TestMethod_GetBadgeByKey()
+        {
+            //Arrange -> Set things up.
+
+            //Act - Do some work
+
+            //Assert check to see if the work is working
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod_DeleteDoor()
+        {
+            //Arrange -> Set things up.
+
+            //Act - Do some work
+
+            //Assert check to see if the work is working
+
+        }
+
+        [TestMethod]
+        public void TestMethod_AddDoor()
+        {
+            //Arrange -> Set things up.
+
+            //Act - Do some work
+
+            //Assert check to see if the work is working
+
+        }
+
+    }
+}
